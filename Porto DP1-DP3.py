1f9f3971fdd525dd0a7e8f88b792cf2a4f2cc7c1
rikza30 <3103120198@student.smktelkom-pwt.sch.id>
2023-04-11 03:46:20 +0700

    ui

--- Porto DP1-DP3.py
+++ Porto DP1-DP3.py
@@ -1,20 +1,46 @@
-from list_function import *
-def fungsiInput():
+import tkinter as tk
 
-    print('<--Selamat Datang-->')
-    print('Silahkan Pilih Menu Dibawah Ini')
-    print('1. Desimal')
-    print('2. Biner')
-    print('3. Oktal')
-    print('4. Hexadesimal')
-    print('5. ASCII')
+def Desimal():
+    string = entry.get()
+    desimal = 0
+    for i in range(len(string)):
+        desimal += ord(string[i]) * (256 ** (len(string)-1-i))
+    result_label.config(text="Hasil konversi ke Desimal adalah : " + str(desimal))
 
-    inputan = int(input("Pilihan anda : "))
+def Biner():
+    string = entry.get()
+    biner = ""
+    for i in range(len(string)):
+        biner += format(ord(string[i]), '08b')
+    result_label.config(text="Hasil konversi ke Biner adalah : " + biner)
 
+def Oktal():
+    string = entry.get()
+    oktal = ""
+    for i in range(len(string)):
+        oktal += format(ord(string[i]), '03o')
+    result_label.config(text="Hasil konversi ke Oktal adalah : " + oktal)
+
+def Hexadesimal():
+    string = entry.get()
+    hexa = ""
+    for i in range(len(string)):
+        hexa += format(ord(string[i]), '02X')
+    result_label.config(text="Hasil konversi ke Hexadesimal adalah : " + hexa)
+
+def ASCII():
+    string = entry.get()
+    ascii_str = "Hasil konversi ke ASCII adalah :\n"
+    for i in range(len(string)):
+        ascii_str += string[i] + " : " + str(ord(string[i])) + "\n"
+    result_label.config(text=ascii_str)
+
+def convert():
+    inputan = option_var.get()
     if inputan == 1 :
         Desimal()
 
-    elif inputan == 2 : 
+    elif inputan == 2 :
         Biner()
 
     elif inputan == 3 :
@@ -27,14 +53,41 @@ def fungsiInput():
         ASCII()
 
     else :
-        print('Mohon masukkan angka sesuai dalam menu!!')
-        print('')
+        result_label.config(text='Mohon masukkan angka sesuai dalam menu!!')
 
-    inputLg = input('Apakah anda ingin melakukan operasi lagi ? (y/n) : ')
+window = tk.Tk()
+window.title("Konversi String")
+window.geometry("500x350")
+window.resizable(False, False)
 
-    if inputLg == 'y' : 
-        fungsiInput()
-    else : 
-        print('Terimakasih')
-        exit
-fungsiInput()
+welcome_label = tk.Label(window, text="<--Selamat Datang-->", font=("Arial", 14))
+welcome_label.pack(pady=10)
+
+option_label = tk.Label(window, text="Silahkan Pilih Menu Dibawah Ini", font=("Arial", 12))
+option_label.pack()
+
+option_var = tk.IntVar()
+option_var.set(1)
+desimal_radiobutton = tk.Radiobutton(window, text="Desimal", variable=option_var, value=1)
+desimal_radiobutton.pack()
+biner_radiobutton = tk.Radiobutton(window, text="Biner", variable=option_var, value=2)
+biner_radiobutton.pack()
+oktal_radiobutton = tk.Radiobutton(window, text="Oktal", variable=option_var, value=3)
+oktal_radiobutton.pack()
+hexa_radiobutton = tk.Radiobutton(window, text="Hexadesimal", variable=option_var, value=4)
+hexa_radiobutton.pack()
+ascii_radiobutton = tk.Radiobutton(window, text="ASCII", variable=option_var, value=5)
+ascii_radiobutton.pack()
+
+entry_label = tk.Label(window, text="Masukkan string :")
+entry_label.pack(pady=10)
+entry = tk.Entry(window)
+entry.pack()
+
+convert_button = tk.Button(window, text="Konversi", command=convert)
+convert_button.pack(pady=10)
+
+result_label = tk.Label(window, font=("Arial", 12))
+result_label.pack()
+
+window.mainloop()
